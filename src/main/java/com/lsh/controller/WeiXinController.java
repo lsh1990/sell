0f90ba24cbac27721b3837585698537dfbea3fba
lsh <lsh>
2018-10-19 09:08:47 +0800

    add 微信controller层开发

--- src/main/java/com/lsh/controller/WeiXinController.java
+++ src/main/java/com/lsh/controller/WeiXinController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * @ClassName WeiXinController
@@ -22,5 +23,10 @@ public class WeiXinController {
     @GetMapping("/auth")
     public void auth(@RequestParam("code") String code) {
         log.info("进入auth方法！");
+        log.info("code={}", code);
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx342df2e01b8146e0&secret=16eb47e1f1985a51e95517ed8db726ae&code=" + code +"&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
     }
 }
